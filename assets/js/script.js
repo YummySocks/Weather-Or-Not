de773a63a201c3ae849b829025b6943b3107dbed
Socks <patricksutcliffe123@gmail.com>
2021-09-25 17:50:56 -0400

    added one call to be able to better grab forecast and current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,28 @@ var ApiKey = '8fceceef7f0be4b04dce5313fe7967a1';
 $('#searchButton').on("click",function(){
     var textInput = $('#searchText').val();
     city = textInput;
-    checkForecast();
+    grabWeatherData();
 })
 
 
-
+//pulls out the lat and lon data to pass on to the real weather call
 function pullWeatherData(d){
-    var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-    console.log(fahrenheit);
+    var lat = d.coord.lat;
+    var lon = d.coord.lon;
+    fullWeatherData(lat,lon);
 }
-
+//used for getting the data for todays weather and the next 5 days
 function forecastData(f){
-    console.log(f.list[6].dt_txt);
+    console.log(f);
 }
 
 
 
 
 
-city = 'Atlanta';
-function checkCurrentWeather(){
-    var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ApiKey;
+//grabs the weather data from the city variable
+function grabWeatherData(){
+    var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ApiKey + '&units=imperial';
     fetch(currentUrl , {
         method: 'GET',
         credentials: 'omit',
@@ -32,10 +33,10 @@ function checkCurrentWeather(){
     .then(response => response.json())
     .then(data => pullWeatherData(data));
 }
-
-function checkForecast(){
-    var foreUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+ApiKey;
-    fetch(foreUrl , {
+//pulls all the data for today and the daily section
+function fullWeatherData(lat,lon){
+    var fullUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=hourly,minutely&appid=' +ApiKey + '&units=imperial';
+    fetch(fullUrl , {
         method: 'GET',
         credentials: 'omit',
         redirect:'follow'
